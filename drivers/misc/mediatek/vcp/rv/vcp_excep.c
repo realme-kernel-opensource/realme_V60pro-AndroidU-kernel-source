f3cfbe5d441d86f0a3958fb307f5a1ccc6d533e0
Chia-Mao Hung <chia-mao.hung@mediatek.com>
2024-01-04 12:18:47 +0800

    [ALPS08653279] vcp: fix reset wake lock issue
    During reset user deregister feature could leak wakelock.
    
    MTK-Commit-Id: d24131c4aefbd7693d43658683a370ec1fb88148
    
    Signed-off-by: Chia-Mao Hung <chia-mao.hung@mediatek.com>
    CR-Id: ALPS08653279
    Feature: Video Driver
    Change-Id: I645834ebaa72fd928bec749a2fe335beaa6426db
    (cherry picked from commit 7cb4ef35838e32da6260f74764bb6861134f31d1)
    (cherry picked from commit 81ff00f558a23c82213c3317d457a30b0532aacb)

--- drivers/misc/mediatek/vcp/rv/vcp_excep.c
+++ drivers/misc/mediatek/vcp/rv/vcp_excep.c
@@ -418,7 +418,7 @@ static unsigned int vcp_crash_dump(enum vcp_core_id id)
 		vcp_awake_fail_flag = 1;
 	}
 	if (vcpreg.secure_dump) {
-		int polling = 1;
+		unsigned long polling = 1;
 		int retry = POLLING_RETRY;
 
 		vcp_do_dump();
@@ -427,6 +427,10 @@ static unsigned int vcp_crash_dump(enum vcp_core_id id)
 			polling = vcp_do_polling();
 			if (!polling)
 				break;
+			if (polling == -EIO) {
+				pr_notice("[VCP] %s: vcp dump polling not support\n", __func__);
+				break;
+			}
 			mdelay(polling);
 			retry--;
 		}
